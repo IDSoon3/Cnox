99e4e2925671beaaa2a128de1fc5a0b652715e1d
IDSoon3 <tripelonex@gmail.com>
2025-07-28 20:35:03 +0700

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -1,8 +1,17 @@
+import Head from 'next/head';
+import dynamic from 'next/dynamic';
+
+const CastAndClaim = dynamic(() => import('../components/CastAndClaim'), { ssr: false });
+
 export default function Home() {
   return (
     <div>
+      <Head>
+        <title>Welcome to Cnox</title>
+      </Head>
       <h1>Welcome to Cnox</h1>
       <p>Airdrop claiming interface using Supabase and Ethers.js</p>
+      <CastAndClaim />
     </div>
   );
 }
